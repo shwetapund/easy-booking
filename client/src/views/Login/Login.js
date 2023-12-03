7f33201d6b82365573dafe1a4c7653be840caf5a
pournimaanjole <pournimaanjole@gmail.com>
2023-12-03 21:00:29 +0530

    styling login and signup page

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,10 +1,41 @@
-import react from 'react';
+import react, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
-function Login(){
-    return(
+function Login() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    return (
         <>
-        <h1>Login</h1>
+            <div className='loginsignup'>
+                <div className='loginsinup-container  '>
+                    <h1>login</h1>
+                    <div className='loginsingup-fields'>
+                        <input type='text'
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value)
+                            }}
+                            placeholder='enter your email'
+                            className='inputfields' />
+
+                        <input type='text'
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}
+                            placeholder='enter your password'
+                            className='inputfields' />
+
+                        <button type='button' >login</button>
+
+                        <p className='loginsignup-login'>Don't have an account? <span className='loginsingup-login-span'><Link to='/signup' className='loginsignup-link'>Signup</Link>
+                        </span></p>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -1,10 +1,64 @@
-import react from 'react';
+import react, { useState } from 'react';
 import './SignUp.css';
+import {Link} from 'react-router-dom'
 
-function SignUp(){
-    return(
+function SignUp() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [mobileNo, setMobileNo] = useState('')
+    const [gender, setGender] = useState('female')
+
+    return (
         <>
-        <h1>SignUp</h1>
+            <div className='loginsignup'>
+                <div className='loginsinup-container'>
+                    <h1>SignUp</h1>
+                    <div className='loginsingup-fields'>
+                        <input type='text'
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value)
+                            }}
+                            placeholder='enter your name'
+                            className='inputfields' />
+
+                        <input type='text'
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value)
+                            }}
+                            placeholder='enter your email'
+                            className='inputfields' />
+
+                        <input type='text'
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}
+                            placeholder='enter your password'
+                            className='inputfields' />
+
+                        <input type='text'
+                            value={mobileNo}
+                            onChange={(e) => {
+                                setMobileNo(e.target.value)
+                            }}
+                            placeholder='enter your mobile no'
+                            className='inputfields' />
+                        <div>
+                            <input type='radio' name='gender' id='male' className='radio'/>
+                            <label htmlFor='male'>male</label>
+
+                            <input type='radio' name='gender' id='female' className='radio' />
+                            <label htmlFor='female' >female</label>
+                        </div>
+                        <button type='button'  >signup</button>
+                        <p className='loginsignup-login'>already have an account? <span className='loginsingup-login-span'><Link to='/login' className='loginsignup-link'>login</Link>
+                        </span></p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
