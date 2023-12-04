6227022c57b803f85fbec5f11bc36f6898120a72
Nitish kumar <krnitish540@gmail.com>
2023-12-04 21:27:59 +0530

    integret Show All Movie

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,11 +1,42 @@
 import './Home.css';
 import Navbar from './../../components/Navbar/Navbar'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import MovieCard from '../../components/MovieCard/MovieCard';
 
 function Home(){
+   const [movie,setMovie]=useState([])
+
+   const loadMovie = async()=>{
+    try{
+        const responce=await axios.get('/api/v1/movies')
+        setMovie(responce?.data?.data)
+    }catch(err){
+        console.error(err.message);
+    }
+   }
+
+
+   useEffect(()=>{
+     loadMovie()
+   },[])
     return(
         <>
         <Navbar/>
-           
+            <div className='show-movie'>
+                {
+                  movie?.map((obj,i)=>{
+                    const {Imageurl,rating,movieType,title}=obj
+                    return <MovieCard
+                    key={i}
+                      Imageurl={Imageurl}
+                      rating={rating}
+                      movieType={movieType}
+                      title={title}
+                    />
+                  })   
+                }
+            </div>
         </>
     )
 }
