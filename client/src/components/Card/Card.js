83714166f96fbaf03afbaf54da6c662c58192ea8
rahuljadhav2163 <rgj4002@gmail.com>
2023-12-15 15:50:50 +0530

    create changes in home page

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Card.css"
 import git from "./../../views/About/git.png"
 import linkdin from "./../../views/About/linkdin.png"
-import peerlist from "./../../views/About/peerlist.jpeg"
+import peerlist from "./../../views/About/peerlistss.jpeg"
 function Card({name,img,description}) {
   return (
     <div className='card-container'>
